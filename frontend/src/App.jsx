87bcab8ab77576a092cd36f8e7843751f2a85025
Raddames <raddamestonui48@gmail.com>
2024-09-13 19:27:11 +0300

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>Welcome to Marps Africa</h1>
+    </div>
+  )
+}
+
+export default App
