0df6476f4ad4caeacb2bbb8ac53ab6dc261ab522
Shadrack <skmutai6@gmail.com>
2024-09-19 18:03:21 +0300

    services update

---
+++ frontend/src/pages/MobiDev.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MobiDev() {
+  return (
+    <div>MobiDev</div>
+  )
+}
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { FaLaptopCode, FaMobileAlt, FaPalette, FaShieldAlt, FaChartBar, FaSms, FaLightbulb, FaIndustry } from 'react-icons/fa';
+import { FaLaptopCode, FaMobileAlt, FaPalette, FaMoneyBillWave, FaChartBar, FaSms, FaLightbulb, FaIndustry } from 'react-icons/fa';
 
 
 function Services() {
   return (
-    <div className=' mx-80'>
+    <div className='mx-10 lg:mx-80'>
       <h1 className=' flex justify-center text-gray-700 font-bold text-3xl pt-8'>Our Services</h1>
-      <div className='grid grid-cols-3 pt-4 gap-4'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 pt-4 gap-4'>
         <div className='relative flex border rounded-lg bg-green-100 hover:bg-green-200 text-green-700 p-2'>
           <div className='flex-grow'>
             Software Development
-            <p className="text-gray-500 text-sm">We plan, design and develop robust software applications.</p>
+            <p className="text-gray-500 text-sm">We plan, design and develop robust software applications that are scalable. Our talented team of developers implements rigorous testing while offering regular maintenance and real-time support.</p>
           </div>
           <div className='absolute top-2 right-2'>
             <FaLaptopCode className="h-6 w-6 text-green-700" />
@@ -21,7 +21,7 @@ function Services() {
           Mobile App Development
             <p className="text-gray-500 text-sm">We develop cross-platform mobile applications.</p>
           </div>
-          <div classname=''>
+          <div className=''>
           <FaMobileAlt className="h-6 w-6 mr-2 text-green-700" />
           </div>
         </div>
@@ -30,7 +30,7 @@ function Services() {
           Website Design
             <p className="text-gray-500 text-sm">Aesthetically pleasing website designs.</p>
           </div>
-          <div classname=''>
+          <div className=''>
           <FaPalette className="h-6 w-6 mr-2 text-green-700" />
           </div>
         </div>
@@ -39,17 +39,17 @@ function Services() {
           Digital Marketing
             <p className="text-gray-500 text-sm">Premier provider of digital marketing.</p>
           </div>
-          <div classname=''>
+          <div className=''>
           <FaChartBar className="h-6 w-6 mr-2 text-green-700" />
           </div>
         </div>
         <div className='relative flex border rounded-lg bg-green-100 hover:bg-green-200 text-green-700 p-2'>
           <div className='flex-grow'>
-          Cybersecurity
-            <p className="text-gray-500 text-sm">Secure your digital platforms.</p>
+          Payment Intergration
+            <p className="text-gray-500 text-sm">Secure payment intergrations for multiple modes of payment. This will ensure smooth checkout while driving more sales and revenue to your business.</p>
           </div>
-          <div classname=''>
-          <FaShieldAlt className="h-6 w-6 mr-2 text-green-700" />
+          <div className=''>
+          <FaMoneyBillWave className="h-6 w-6 mr-2 text-green-700" />
           </div>
         </div>
         <div className='relative flex border rounded-lg bg-green-100 hover:bg-green-200 text-green-700 p-2'>
@@ -57,7 +57,7 @@ function Services() {
           Bulk SMS
             <p className="text-gray-500 text-sm">Quick and easy integration Bulk SMS service.</p>
           </div>
-          <div classname=''>
+          <div className=''>
           <FaSms className="h-6 w-6 mr-2 text-green-700" />
           </div>
         </div>
@@ -66,17 +66,8 @@ function Services() {
           Research & Innovations
             <p className="text-gray-500 text-sm">We help companies to conduct extensive research and innovations.</p>
           </div>
-          <div classname=''>
+          <div className=''>
           <FaLightbulb className="h-6 w-6 mr-2 text-green-700" />
-          </div>
-        </div>
-        <div className='relative flex border rounded-lg bg-green-100 hover:bg-green-200 text-green-700 p-2'>
-          <div className='flex-grow'>
-          Industry Training
-            <p className="text-gray-500 text-sm">Professional training on digital solutions.</p>
-          </div>
-          <div>
-          <FaIndustry className="h-6 w-6 mr-2 text-green-700" />
           </div>
         </div>
 
