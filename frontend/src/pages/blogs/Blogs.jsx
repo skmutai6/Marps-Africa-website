64ff8793885782a35d9623df317e4ab9ed06ed6f
spookeyy <pangasmeshack@gmail.com>
2024-09-20 16:23:12 +0300

    blog pages folder

--- frontend/src/pages/blogs/Blogs.jsx
+++ frontend/src/pages/blogs/Blogs.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import blogData from "../mockData.json";
+import blogData from "../../mockData.json";
 
 function Blogs() {
   return (
